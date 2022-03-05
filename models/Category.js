4707d836c210804b731577641f7eab48f7994220
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-03-05 15:31:30 -0700

    Init repo and worked on models

---
+++ models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      refernces: {
+        model: 'product',
+        key: 'category_id',
+      }
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
---
+++ models/Tag.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      refernces: {
+        model: 'product_tag',
+        key: 'tag_id',
+      }
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
